f8f3c6aa056b2eac4fbaf632bbca2605a0ecec3f
Polina <106979919+kollarbone@users.noreply.github.com>
2022-12-06 17:17:50 +0400

    rwerw

--- src/Components/Tasks/TaskDetail/TaskDetail.jsx
+++ src/Components/Tasks/TaskDetail/TaskDetail.jsx
@@ -3,13 +3,17 @@ import "./TaskDetail.css";
 import "draft-js/dist/Draft.css";
 import { MdOutlineAdd } from "react-icons/md";
 import { VscClose } from "react-icons/vsc";
+import { AiOutlineEdit } from "react-icons/ai";
 
 export default function TaskDetail(props) {
   const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
+  const [name, setName] = useState(props.item.taskName);
+  const [descr, setDescr] = useState(props.item.taskDescr);
   const [sub, setSub] = useState("");
   const [onCkick, setOnClick] = useState(false);
   const [subs, setSubs] = useState(props.item ? props.item.sub : "");
+  const [isEditClicked, setIsEdutClicked] = useState(false);
 
   const onClickHandlerSub = (e) => {
     setSubs((subs) => [...subs, { name: sub }]);
@@ -25,9 +29,22 @@ export default function TaskDetail(props) {
   const onChangeHandler = (e) => {
     setComment(e.target.value);
   };
+  const onChangeHandlerName = (e) => {
+    setName(e.target.value);
+  };
+  const onChangeHandlerDescr = (e) => {
+    setDescr(e.target.value);
+  };
   const startDate = new Date(props.item.start);
   const timeEnd = new Date();
   const diff = new Date(timeEnd.getTime() - startDate.getTime());
+  const handleEditClick = () => {
+    if (isEditClicked === false) {
+      setIsEdutClicked(true);
+    } else if (isEditClicked === true) {
+      setIsEdutClicked(false);
+    }
+  };
 
   return (
     <div className="task_detail" onClick={props.onClose}>
@@ -36,14 +53,33 @@ export default function TaskDetail(props) {
         style={{ backgroundColor: "#e1e6f9" }}
         onClick={(e) => e.stopPropagation()}
       >
-        <span className="task_name">
-          {props.item.taskNumber}.{props.item.taskName}
-        </span>
+        {isEditClicked === false && (
+          <span className="task_name">
+            {props.item.taskNumber}.{name}
+            <AiOutlineEdit className="add_icon" onClick={handleEditClick} />
+          </span>
+        )}
+        {isEditClicked === true && (
+          <span className="task_name">
+            <input value={name} onChange={onChangeHandlerName} />
+            <AiOutlineEdit className="add_icon" onClick={handleEditClick} />
+          </span>
+        )}
         <div className="info_block">
           <div className="full_task_info">
-            <span className="task_descr">
-              Description: {props.item.taskDescr}
-            </span>
+            {isEditClicked === false && (
+              <span className="task_descr">Description: {descr}</span>
+            )}
+            {isEditClicked === true && (
+              <span className="task_descr">
+                Description:
+                <input
+                  style={{ width: "400px" }}
+                  value={descr}
+                  onChange={onChangeHandlerDescr}
+                />
+              </span>
+            )}
             <span className="task_duration">
               Creation data: {props.item.start}
             </span>
